023689373a134b39a1c280d6cca6eb461afdc4d0
joon Park <someon@yahoo.com>
2016-12-22 23:26:25 -0500

    Revert "make a mistake"
    This reverts commit be98b9cb9d912d96790551cb74d501512a915bc7.

--- RevertThis/src/Randomclass.java
+++ RevertThis/src/Randomclass.java
@@ -4,6 +4,5 @@ public class Randomclass {
 	public static void main(String args[]){
 		System.out.println("first part");
 		System.out.println("second part");
-		System.out.println("mistake");
 	}
 }
