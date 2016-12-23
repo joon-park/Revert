45ee817639568548698638abeb2584336454aff0
joon Park <someon@yahoo.com>
2016-12-22 23:39:25 -0500

    Revert "Merge branch 'revertMerge2' into revertMerge"
    This reverts commit 5a1aa6773e412cb27e0a480d2eb56acc38db4f5c, reversing
    changes made to 3d4775a940193f22811ba70fc89316dc7c45dbfe.
    
    Reverted the merge of revertmerg2 into revertmerge

--- RevertThis/src/Randomclass.java
+++ RevertThis/src/Randomclass.java
@@ -4,7 +4,5 @@ public class Randomclass {
 	public static void main(String args[]){
 		System.out.println("first part");
 		System.out.println("second part");
-		System.out.println("mistake I want to take out");
-		System.out.println("fourth part");
 	}
 }
